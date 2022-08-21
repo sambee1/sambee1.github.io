225433e78b8c5b939d073517e357eecaa4f697e2
Samuel Nnaji <61295743+sambee1@users.noreply.github.com>
2022-08-21 07:05:07 +0100

    Initial commit

---
+++ Portfolio site/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+window.onscroll = function () {
+	var header = document.querySelector(".navigation");
+	var sticky = header.offsetTop;
+
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+
+
+
+	// show or hide the back-top-top button
+	var backToTo = document.querySelector(".scroll-top");
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		backToTo.style.display = "block";
+	} else {
+		backToTo.style.display = "none";
+	}
+};
+
+function onScroll(event) {
+	var sections = document.querySelectorAll('.page-scroll');
+	var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+	for (var i = 0; i < sections.length; i++) {
+		var currLink = sections[i];
+		var val = currLink.getAttribute('href');
+		var refElement = document.querySelector(val);
+		var scrollTopMinus = scrollPos + 20;
+		if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+			document.querySelector('.page-scroll').classList.remove('active');
+			currLink.classList.add('active');
+		} else {
+			currLink.classList.remove('active');
+		}
+	}
+};
+
+window.document.addEventListener('scroll', onScroll);
